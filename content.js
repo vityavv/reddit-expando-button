2eff80448d284e03048f6581077bd3fcd84fbe5b
Victor Veytsman <vityavv@gmail.com>
2017-11-19 10:30:15 -0500

    Add actual script and specify in manifest

---
+++ content.js
@@ -0,0 +1,6 @@
+//javascript:for (var i = 0; i < document.getElementsByClassName("expando-button").length; i++) {document.getElementsByClassName("expando-button")[i].click()}
+window.onload = function() {
+  for (var i = 0; i < document.getElementsByClassName("expando-button").length; i++) {
+    document.getElementsByClassName("expando-button")[i].click();
+  }
+}
